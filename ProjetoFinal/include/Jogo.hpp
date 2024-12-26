10b5bc8b962152c625f7f55496275d9342ca2c62
PedLuiz <ped.luiz20@gmail.com>
2024-12-26 20:16:55 -0300

    Adicionado comentários explicando as funções a serem implementadas

--- ProjetoFinal/include/Jogo.hpp
+++ ProjetoFinal/include/Jogo.hpp
@@ -12,20 +12,31 @@ protected:
     vector<vector<char>> tabuleiro;
 
 public:
+    /* Construtor */
     Jogo (int M, int N); 
 
-    virtual vector<pair<int,int>> getJogadas() = 0;
-
+    // Funcao que retorna qual jogador venceu 'X' 'O' ou '' (nenhum)
     virtual char getVencedor() = 0;
 
+    // Funcao retorna verdadeiro se o jogo terminou
     virtual bool isEstadoFinal() = 0;
 
+    // Funcao retorna qual jogador tem o turno 'X' ou 'O'
     virtual char getTurno() = 0;
 
+    // Funcao recebe um par de coordenadas (i, j) e se for uma jogada valida, atualiza o tabuleiro, conforme o jogador q tem o turno
     virtual void fazerJogada(pair<int, int> jogada) = 0;
 
+    /* Retorna verdadeiro se a jogada é valida
+    checar:
+        movimento dentro do tabuleiro
+        movimento em uma casa já preenchida
+        movimento contraria alguma regra especifica do jogo
+     */
     virtual bool isJogadaValida(pair<int,int> jogada) = 0;
 
+    // Printa o tabuleiro, formatação do pdf, tem uma implementação padrao (ver PDF do TP), mas pode ser sobrecarregada
+    // a depender do jogo
     virtual void printTabuleiro();
 };
 
