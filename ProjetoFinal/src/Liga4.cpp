0cc32d9dcb146c3bf2c69069360c3e060722f516
Marcos Aurelio <marcos92santos47@gmail.com>
2024-12-30 14:20:15 -0300

    Definição inicial do cabeçalho e implementação de Liga4

---
+++ ProjetoFinal/src/Liga4.cpp
@@ -0,0 +1,18 @@
+#include "../include/Liga4.hpp"
+
+using namespace std;
+
+    Liga4::Liga4 () : Jogo(7, 6){} 
+
+    char getVencedor() {}
+
+    bool isEstadoFinal() {}
+
+    char getTurno() {}
+
+    void fazerJogada(pair<int, int> jogada) {}
+
+    bool isJogadaValida(pair<int,int> jogada) {}
+
+    Liga4::~Liga4(){}
+
